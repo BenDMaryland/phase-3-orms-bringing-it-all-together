c1cd8a9d7b64b42afbe9da716c1419bb222aee23
ben.d.maryland@gmail.com <bend@RoseFan.localdomain>
2021-11-04 12:07:22 -0400

    commit tree 9e65e9f752a2b51d288d4b784e0e445623a6b9c7

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,42 @@
 class Dog
 
+    attr_accessor :name, :breed, :id
+    
+    def initialize(name:, breed:, id: nil)
+        @id = id
+        @name = name
+        @breed = breed
+      end
+
+def self.create_table
+    sql = <<-SQL
+    CREATE TABLE IF NOT EXISTS dogs (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        breed TEXT
+
+    )
+SQL
+DB[:conn].execute(sql)
+end
+
+
+
+def self.drop_tables
+ sql = <<-SQL
+ DROP TABLE  dog;
+
+
+SQL
+
+DB[:conn].execute(sql)
+
+end
+
+
+
+
+
+
+
 end
